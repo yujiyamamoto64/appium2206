fba7d0de45378739a967bcaa5bd2e02e0b4fef29
yujiyamamoto64 <yujiyamamoto64@gmail.com>
2022-06-10 10:34:37 -0300

    Get text method on mobile

---
+++ src/test/java/appium2206/apilearning/GetValue.java
@@ -0,0 +1,34 @@
+package appium2206.apilearning;
+
+import appium2206.utils.AppiumDriverEx;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+
+public class GetValue {
+
+	public static void main(String[] args) {
+		
+		// 1. Launch the target app
+		AppiumDriver<MobileElement> appiumDriver = AppiumDriverEx.getAppiumDriver();
+		
+		// 2. Click on Login Label
+		MobileElement loginLabel = appiumDriver.findElementByAccessibilityId("Login");
+		loginLabel.click();
+		
+		// 3. input username
+		MobileElement emailTxtBx = appiumDriver.findElementByAccessibilityId("input-email");
+		emailTxtBx.sendKeys("yujiyamamoto64@gmail.com");
+		
+		// 4. input password
+		MobileElement passwordTxtBx = appiumDriver.findElementByAccessibilityId("input-password");
+		passwordTxtBx.sendKeys("password");
+		
+		// 5. click on login button
+		MobileElement loginBtn = appiumDriver.findElementByAccessibilityId("button-LOGIN");
+		loginBtn.click();
+		
+		// 6. Get the title from the dialog
+		MobileElement successTitle = appiumDriver.findElementById("android:id/alertTitle");
+		System.out.println("Dialog title: " + successTitle.getText());
+	}
+}
